c1160082287a63a420b7334fce379fd50454de07
Neelanjana Mukherjee <neelanjanamukherjee@zipcoders-MacBook-Pro.local>
2019-04-01 14:44:36 -0400

    word

--- src/main/java/rocks/zipcode/quiz4/collections/WordCounter.java
+++ src/main/java/rocks/zipcode/quiz4/collections/WordCounter.java
@@ -1,12 +1,32 @@
 package rocks.zipcode.quiz4.collections;
 
 import java.util.Map;
+import java.util.TreeMap;
 
 public class WordCounter {
+
+    String[] strings;
+
     public WordCounter(String... strings) {
+
+        this.strings = strings;
     }
 
     public Map<String, Integer> getWordCountMap() {
-        return null;
+
+        Map<String, Integer> map = new TreeMap<>();
+
+        for(int i=0;i<strings.length; i++){
+
+            if(!(map.containsKey(strings[i]))){
+                map.put(strings[i], 1);
+            }
+
+           else{
+               map.put(strings[i], map.get(strings[i])+1);
+            }
+        }
+
+        return map;
     }
 }
--- src/main/java/rocks/zipcode/quiz4/fundamentals/StringUtils.java
+++ src/main/java/rocks/zipcode/quiz4/fundamentals/StringUtils.java
@@ -5,15 +5,28 @@ package rocks.zipcode.quiz4.fundamentals;
  */
 public class StringUtils {
     public static Character getMiddleCharacter(String string) {
-        return null;
+
+        return string.charAt(string.length()/2);
     }
 
     public static String capitalizeMiddleCharacter(String str) {
-        return null;
+
+        String result = "";
+        String prefix = str.substring(0, str.length()/2);
+        String toCapitalize = getMiddleCharacter(str).toString().toUpperCase();
+        String suffix = str.substring((str.length()/2)+1);
+        result = prefix+toCapitalize+suffix;
+        return result;
     }
 
     public static String lowerCaseMiddleCharacter(String str) {
-        return null;
+
+        String result = "";
+        String prefix = str.substring(0, str.length()/2);
+        String toCapitalize = getMiddleCharacter(str).toString().toLowerCase();
+        String suffix = str.substring((str.length()/2)+1);
+        result = prefix+toCapitalize+suffix;
+        return result;
     }
 
     public static Boolean isIsogram(String str) {
@@ -21,14 +34,25 @@ public class StringUtils {
     }
 
     public static Boolean hasDuplicateConsecutiveCharacters(String str) {
+
+
         return null;
     }
 
     public static String removeConsecutiveDuplicateCharacters(String str) {
+
+
         return null;
     }
 
     public static String invertCasing(String str) {
+
+        for(int i =0; i<str.length();i++) {
+            if (Character.isUpperCase(str.charAt(i))){
+
+            }
+        }
+
         return null;
     }
 }
