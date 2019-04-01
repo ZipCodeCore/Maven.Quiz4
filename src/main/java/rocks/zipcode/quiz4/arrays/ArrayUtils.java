8443d35a5fb352529bd703abd2bca5dee79e8cb3
Aswathy Kanakarajan <aswathykanakarajan@zipcoders-MacBook-Pro-7.local>
2019-04-01 09:44:20 -0400

    Arrays implemented and tested

--- src/main/java/rocks/zipcode/quiz4/arrays/ArrayUtils.java
+++ src/main/java/rocks/zipcode/quiz4/arrays/ArrayUtils.java
@@ -1,22 +1,31 @@
 package rocks.zipcode.quiz4.arrays;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * @author leon on 01/01/2019.
  */
 public class ArrayUtils {
     public static String getMiddleElement(String[] values) {
-        return null;
+        return values[values.length/2];
     }
 
     public static String[] removeMiddleElement(String[] values) {
-        return null;
+        String middle = getMiddleElement(values);
+        List<String> list = Arrays.stream(values)
+                .filter(value -> !value.equals(middle))
+                .collect(Collectors.toList());
+
+        return list.toArray(new String[list.size()]);
     }
 
     public static String getLastElement(String[] values) {
-        return null;
+        return values[values.length - 1];
     }
 
     public static String[] removeLastElement(String[] values) {
-        return null;
+        return Arrays.copyOf(values, values.length - 1);
     }
 }
