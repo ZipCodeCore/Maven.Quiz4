d0b7f25f1fc4a71440871bce5231f57b0b96b084
Ashley Bloxom <ashley.bloxom@gmail.com>
2019-04-01 10:34:55 -0400

    finished account and bankaccount

--- src/main/java/rocks/zipcode/quiz4/objectorientation/account/BankAccount.java
+++ src/main/java/rocks/zipcode/quiz4/objectorientation/account/BankAccount.java
@@ -3,7 +3,44 @@ package rocks.zipcode.quiz4.objectorientation.account;
 /**
  * @author leon on 27/12/2018.
  */
-public class BankAccount {
+public class BankAccount extends Account implements Transactable{
+
+    private Double balance;
+
+    public BankAccount() {
+        super();
+        this.balance = 0.0;
+    }
+
+    public BankAccount(Long id) {
+        super(id);
+        this.balance = 0.0;
+    }
+
     public void setBalance(Double val) {
+        this.balance = val;
+    }
+
+    @Override
+    public void deposit(Double amountToIncreaseBy) {
+        if (amountToIncreaseBy >= 0) {
+            balance += amountToIncreaseBy;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    @Override
+    public void withdrawal(Double amountToDecreaseBy) {
+        if (amountToDecreaseBy < 0 || amountToDecreaseBy > balance) {
+            throw new IllegalArgumentException();
+        } else {
+            balance -= amountToDecreaseBy;
+        }
+    }
+
+    @Override
+    public Double getBalance() {
+        return balance;
     }
 }
