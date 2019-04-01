686677d9272f433746249bf4a2026305576cb182
Nirmala <nirmala.talari@gmail.com>
2019-04-01 12:03:19 -0400

    BankTestsDone

--- src/main/java/rocks/zipcode/quiz4/objectorientation/account/Bank.java
+++ src/main/java/rocks/zipcode/quiz4/objectorientation/account/Bank.java
@@ -1,22 +1,35 @@
 package rocks.zipcode.quiz4.objectorientation.account;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author leon on 27/12/2018.
  */
 public class Bank {
 
+    List<BankAccount> bankAccountsList;
 
     public Bank() {
+        bankAccountsList = new ArrayList<>();
     }
 
-    public BankAccount removeBankAccountByIndex(Integer indexNumber) {
-        return null;
+    public BankAccount removeBankAccountByIndex(int indexNumber) {
+        BankAccount acctRemoved = this.bankAccountsList.get(indexNumber);
+        this.bankAccountsList.remove(indexNumber);
+        return acctRemoved;
     }
 
     public void addBankAccount(BankAccount bankAccount) {
+        this.bankAccountsList.add(bankAccount);
     }
 
     public Boolean containsBankAccount(BankAccount bankAccount) {
-        throw new UnsupportedOperationException("Method not yet implemented");
+        if (bankAccountsList.contains(bankAccount))
+            return true;
+        else
+            return false;
+
+       // throw new UnsupportedOperationException("Method not yet implemented");
     }
 }
--- src/main/java/rocks/zipcode/quiz4/objectorientation/account/BankAccount.java
+++ src/main/java/rocks/zipcode/quiz4/objectorientation/account/BankAccount.java
@@ -3,11 +3,14 @@ package rocks.zipcode.quiz4.objectorientation.account;
 /**
  * @author leon on 27/12/2018.
  */
-public class BankAccount {
+public class BankAccount extends Bank {
+    Double balance = 0.0;
 
     public BankAccount() {
+        super();
     }
 
     public void setBalance(Double val) {
+        this.balance = val;
     }
 }
