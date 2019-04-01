f80659e6742b7ac673901624269b70083670061c
Brian Wong <brianwong@zipcoders-MacBook-Pro.local>
2019-04-01 13:12:26 -0400

    Need to finish duplices and spice count

--- src/main/java/rocks/zipcode/quiz4/arrays/ArrayUtils.java
+++ src/main/java/rocks/zipcode/quiz4/arrays/ArrayUtils.java
@@ -1,22 +1,47 @@
 package rocks.zipcode.quiz4.arrays;
 
+import rocks.zipcode.quiz4.fundamentals.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author leon on 01/01/2019.
  */
 public class ArrayUtils {
     public static String getMiddleElement(String[] values) {
-        return null;
+        Integer length = values.length;
+        if(length % 2 == 1) {
+            length = length/2;
+        }
+        else {
+            length = (length/2) -1;
+        }
+        return values[length];
     }
 
     public static String[] removeMiddleElement(String[] values) {
-        return null;
+        String middleElement = getMiddleElement(values);
+        List<String> stringArrayList = new ArrayList<>();
+        for (int i = 0; i < values.length ; i++) {
+            if(values[i] != middleElement) {
+                stringArrayList.add(values[i]);
+            }
+        }
+        String[] stringArray = stringArrayList.stream().toArray(String[]::new);
+        return stringArray;
     }
 
     public static String getLastElement(String[] values) {
-        return null;
+        Integer lastElement = values.length -1;
+        return values[lastElement];
     }
 
     public static String[] removeLastElement(String[] values) {
-        return null;
+        String[] newArray = new String[values.length -1];
+        for (int i = 0; i < values.length-1 ; i++) {
+            newArray[i] = values[i];
+        }
+        return newArray;
     }
 }
--- src/main/java/rocks/zipcode/quiz4/collections/culonary/Food.java
+++ src/main/java/rocks/zipcode/quiz4/collections/culonary/Food.java
@@ -1,5 +1,7 @@
 package rocks.zipcode.quiz4.collections.culonary;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -7,14 +9,29 @@ import java.util.Map;
  * @author leon on 27/12/2018.
  */
 public class Food {
+    public List<Spice> listOfSpices;
+
+    public Food() {
+        this.listOfSpices = new ArrayList<>();
+    }
+
+    public Food(List<Spice> spices) {
+        this.listOfSpices = spices;
+    }
     public List<Spice> getAllSpices() {
-        return null;
+        return listOfSpices;
     }
 
     public <SpiceType extends Class<? extends Spice>> Map<SpiceType, Integer> getSpiceCount() {
+        Map<SpiceType,Integer> spiceMap = new HashMap<>();
+        for (Spice spice: listOfSpices) {
+            if(spiceMap.get(spice) == null) {
+            }
+        }
         return null;
     }
 
     public void applySpice(Spice spice) {
+        listOfSpices.add(spice);
     }
 }
