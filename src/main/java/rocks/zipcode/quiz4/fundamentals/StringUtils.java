003bd208082c8ca2d7aa98c14321e04329e4d0c0
WilliamHXu <william.haofei.xu@gmail.com>
2019-04-01 10:57:33 -0400

    Completed quiz 4

--- src/main/java/rocks/zipcode/quiz4/fundamentals/StringUtils.java
+++ src/main/java/rocks/zipcode/quiz4/fundamentals/StringUtils.java
@@ -1,34 +1,77 @@
 package rocks.zipcode.quiz4.fundamentals;
 
+import java.util.HashSet;
+
 /**
  * @author leon on 21/12/2018.
  */
 public class StringUtils {
     public static Character getMiddleCharacter(String string) {
-        return null;
+        return string.charAt(string.length()/2);
     }
 
     public static String capitalizeMiddleCharacter(String str) {
-        return null;
+        int index = str.length()/2;
+        return str.substring(0, index) + ((Character)str.charAt(index)).toString().toUpperCase() + str.substring(index + 1);
     }
 
     public static String lowerCaseMiddleCharacter(String str) {
-        return null;
+        int index = str.length()/2;
+        return str.substring(0, index) + ((Character)str.charAt(index)).toString().toLowerCase() + str.substring(index + 1);
     }
 
     public static Boolean isIsogram(String str) {
-        return null;
+        HashSet<Character> characters = new HashSet<>();
+        for (int i = 0; i < str.length(); i++) {
+            if(!characters.add(str.charAt(i))){
+                return false;
+            }
+        }
+        return true;
     }
 
     public static Boolean hasDuplicateConsecutiveCharacters(String str) {
-        return null;
+        if (str.length() > 1) {
+            for (int i = 1; i < str.length(); i++) {
+                if (str.charAt(i) == str.charAt(i - 1)) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     public static String removeConsecutiveDuplicateCharacters(String str) {
-        return null;
+        if (str.length() > 1) {
+            String result = "";
+            for (int i = 0; i < str.length() - 1; i++) {
+                if (!(str.charAt(i) == str.charAt(i + 1))){
+                    result += str.charAt(i);
+                }
+                else i++;
+            }
+            if (!(str.charAt(str.length() - 2) == str.charAt(str.length() - 1))){
+                result += str.charAt(str.length() - 1);
+            }
+            return result;
+        }
+        return str;
     }
 
     public static String invertCasing(String str) {
-        return null;
+        String result = "";
+        for (int i = 0; i < str.length(); i++) {
+            Character current = str.charAt(i);
+            if (current >= 'a' && current <= 'z') {
+                result += current.toString().toUpperCase();
+            }
+            else if (current >= 'A' && current <= 'Z') {
+                result += current.toString().toLowerCase();
+            }
+            else {
+                result += current;
+            }
+        }
+        return result;
     }
 }
--- src/main/java/rocks/zipcode/quiz4/objectorientation/account/Bank.java
+++ src/main/java/rocks/zipcode/quiz4/objectorientation/account/Bank.java
@@ -1,17 +1,30 @@
 package rocks.zipcode.quiz4.objectorientation.account;
 
+import java.util.ArrayList;
+
 /**
  * @author leon on 27/12/2018.
  */
 public class Bank {
-    public BankAccount removeBankAccountByIndex(Integer indexNumber) {
-        return null;
+    private ArrayList<BankAccount> accounts;
+
+    public Bank(ArrayList<BankAccount> accounts) {
+        this.accounts = accounts;
+    }
+
+    public Bank() {
+        this.accounts = new ArrayList<>();
+    }
+
+    public BankAccount removeBankAccountByIndex(int indexNumber) {
+        return accounts.remove(indexNumber);
     }
 
     public void addBankAccount(BankAccount bankAccount) {
+        accounts.add(bankAccount);
     }
 
     public Boolean containsBankAccount(BankAccount bankAccount) {
-        throw new UnsupportedOperationException("Method not yet implemented");
+        return accounts.contains(bankAccount);
     }
 }
