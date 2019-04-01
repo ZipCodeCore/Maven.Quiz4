4e929b7204a93db01fc5a28b29d5b7a22c6b823c
Julian Rios <julianrios@zipcoders-MacBook-Pro-3.local>
2019-04-01 12:34:54 -0400

    completed all tests for employee and all but one in bankAccount

--- src/main/java/rocks/zipcode/quiz4/objectorientation/account/Bank.java
+++ src/main/java/rocks/zipcode/quiz4/objectorientation/account/Bank.java
@@ -1,17 +1,31 @@
 package rocks.zipcode.quiz4.objectorientation.account;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author leon on 27/12/2018.
  */
 public class Bank extends Account{
+    private List<BankAccount> accounts = new ArrayList<>();
+
     public BankAccount removeBankAccountByIndex(Integer indexNumber) {
+        System.out.println(this.accounts.remove(indexNumber));
+//        for(int i =0; i < accounts.size(); i++) {
+//            if()
+//        }
         return null;
     }
 
     public void addBankAccount(BankAccount bankAccount) {
+        this.accounts.add(bankAccount);
     }
 
     public Boolean containsBankAccount(BankAccount bankAccount) {
-        throw new UnsupportedOperationException("Method not yet implemented");
+        if(this.accounts.contains(bankAccount)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
