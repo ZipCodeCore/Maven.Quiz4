ff7db2b5475e30fddb20b28fbb64ee9773d864c3
Michael Krohn <michaelkrohn@MacBook-Pro-8.localdomain>
2019-04-01 14:00:04 -0400

    Refactored map in food to list

--- src/main/java/rocks/zipcode/quiz4/collections/culonary/Food.java
+++ src/main/java/rocks/zipcode/quiz4/collections/culonary/Food.java
@@ -1,34 +1,31 @@
 package rocks.zipcode.quiz4.collections.culonary;
 
-import rocks.zipcode.quiz4.collections.WordCounter;
-
 import java.util.*;
 
 /**
  * @author leon on 27/12/2018.
  */
 public class Food {
-    private Map<Spice,Integer> spiceMap;
+    private List<Spice> spiceList;
 
     public Food() {
-        this.spiceMap = new LinkedHashMap<>();
+        this.spiceList = new ArrayList<>();
     }
 
     public List<Spice> getAllSpices() {
-        return new ArrayList<>(spiceMap.keySet());
+        return spiceList;
     }
 
     public <SpiceType extends Class<? extends Spice>> Map<SpiceType, Integer> getSpiceCount() {
         Map<SpiceType,Integer> spiceTypeIntegerMap = new HashMap<>();
 
-        spiceMap.keySet().forEach(spice -> spiceTypeIntegerMap.putIfAbsent((SpiceType) spice.getClass(),0));
-        spiceMap.keySet().forEach(spice -> spiceTypeIntegerMap.put((SpiceType) spice.getClass(),spiceTypeIntegerMap.get(spice.getClass())+1));
+        spiceList.forEach(spice -> spiceTypeIntegerMap.putIfAbsent((SpiceType) spice.getClass(),0));
+        spiceList.forEach(spice -> spiceTypeIntegerMap.put((SpiceType) spice.getClass(),spiceTypeIntegerMap.get(spice.getClass())+1));
 
         return spiceTypeIntegerMap;
     }
 
     public void applySpice(Spice spice) {
-        spiceMap.putIfAbsent(spice,0);
-        spiceMap.put(spice,spiceMap.get(spice)+1);
+        spiceList.add(spice);
     }
 }
