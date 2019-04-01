be6637f9fb0bb2d2bda88278de8b3d35b890acd3
Alexandra Bozzini <alexandrabozzini@zipcoders-MacBook-Pro-74.local>
2019-04-01 11:25:09 -0400

    Finished food/spice

--- src/main/java/rocks/zipcode/quiz4/collections/culonary/Food.java
+++ src/main/java/rocks/zipcode/quiz4/collections/culonary/Food.java
@@ -1,5 +1,7 @@
 package rocks.zipcode.quiz4.collections.culonary;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -7,14 +9,34 @@ import java.util.Map;
  * @author leon on 27/12/2018.
  */
 public class Food {
+    private List<Spice> spices;
+
+    public Food(List<Spice> spices){
+        this.spices = spices;
+    }
+
+    public Food(){
+        this.spices = new ArrayList<>();
+    }
+
     public List<Spice> getAllSpices() {
-        return null;
+        return this.spices;
     }
 
     public <SpiceType extends Class<? extends Spice>> Map<SpiceType, Integer> getSpiceCount() {
-        return null;
+        Map<SpiceType, Integer> spiceCount = new HashMap<>();
+        for (Spice spice : spices){
+            if (!spiceCount.containsKey((SpiceType) spice.getClass())){
+                spiceCount.put((SpiceType) spice.getClass(), 1);
+            } else {
+                Integer count = spiceCount.get((SpiceType) spice.getClass());
+                spiceCount.put((SpiceType) spice.getClass(), count + 1);
+            }
+        }
+        return spiceCount;
     }
 
     public void applySpice(Spice spice) {
+        spices.add(spice);
     }
 }
