4dc527d3e5a88d003052f42c1ada0093a5bbd3e8
Eleonor Bart <eleonorbart@zipcoders-MacBook-Pro-51.local>
2019-04-01 10:48:18 -0400

    Passed all tests

--- src/main/java/rocks/zipcode/quiz4/collections/WordCounter.java
+++ src/main/java/rocks/zipcode/quiz4/collections/WordCounter.java
@@ -1,12 +1,25 @@
 package rocks.zipcode.quiz4.collections;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class WordCounter {
+    private Map<String, Integer> stringIntegerMap;
     public WordCounter(String... strings) {
+        stringIntegerMap = new HashMap<>();
+        getFrequencies(strings);
+    }
+
+    private void getFrequencies(String[] strings) {
+        for (String string : strings) {
+            if (!stringIntegerMap.containsKey(string)) {
+                stringIntegerMap.put(string, 0);
+            }
+            stringIntegerMap.put(string, stringIntegerMap.get(string) + 1);
+        }
     }
 
     public Map<String, Integer> getWordCountMap() {
-        return null;
+        return stringIntegerMap;
     }
 }
--- src/main/java/rocks/zipcode/quiz4/collections/culonary/Food.java
+++ src/main/java/rocks/zipcode/quiz4/collections/culonary/Food.java
@@ -1,20 +1,52 @@
 package rocks.zipcode.quiz4.collections.culonary;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author leon on 27/12/2018.
  */
 public class Food {
+    private Map<Spice, Integer> spiceIntegerMap;
+
+    public Food() {
+        this.spiceIntegerMap = new LinkedHashMap<>();
+    }
+
+    public Food(List<Spice> spices) {
+        this();
+        getSpiceFrequencies(spices);
+    }
+
+    private void getSpiceFrequencies(List<Spice> spices) {
+        for (Spice spice : spices) {
+            applySpice(spice);
+        }
+    }
+
     public List<Spice> getAllSpices() {
-        return null;
+        return new ArrayList<>(spiceIntegerMap.keySet());
     }
 
     public <SpiceType extends Class<? extends Spice>> Map<SpiceType, Integer> getSpiceCount() {
-        return null;
+        return mapTypes();
+    }
+
+    private <SpiceType extends Class<? extends Spice>> Map<SpiceType, Integer> mapTypes() {
+        Map<SpiceType, Integer> spiceTypeIntegerMap = new HashMap<>();
+        for (Spice spice : spiceIntegerMap.keySet()) {
+            SpiceType spiceType = (SpiceType) spice.getClass();
+            if (!spiceTypeIntegerMap.containsKey(spiceType)) {
+                spiceTypeIntegerMap.put(spiceType, 0);
+            }
+            spiceTypeIntegerMap.put(spiceType, spiceTypeIntegerMap.get(spiceType) + 1);
+        }
+        return spiceTypeIntegerMap;
     }
 
     public void applySpice(Spice spice) {
+        if (!spiceIntegerMap.containsKey(spice)) {
+            spiceIntegerMap.put(spice, 0);
+        }
+        spiceIntegerMap.put(spice, spiceIntegerMap.get(spice) + 1);
     }
 }
