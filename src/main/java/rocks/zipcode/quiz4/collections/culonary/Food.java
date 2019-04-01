245f32ef78b33a8e7fce3a7d07f956488305934c
Cara Eppes <ceppes@udel.edu>
2019-04-01 12:13:38 -0400

    finished collections

--- src/main/java/rocks/zipcode/quiz4/collections/culonary/Food.java
+++ src/main/java/rocks/zipcode/quiz4/collections/culonary/Food.java
@@ -1,20 +1,43 @@
 package rocks.zipcode.quiz4.collections.culonary;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author leon on 27/12/2018.
  */
 public class Food {
+
+    private List<Spice> spicesApplied;
+
+    public Food() {
+        this.spicesApplied = new ArrayList<>();
+    }
+
     public List<Spice> getAllSpices() {
-        return null;
+        return spicesApplied;
     }
 
     public <SpiceType extends Class<? extends Spice>> Map<SpiceType, Integer> getSpiceCount() {
-        return null;
+
+        Map<Class<? extends Spice>, Integer> spiceMap = new HashMap<>();
+        for (Spice spice : spicesApplied) {
+            Class<? extends Spice> spiceClass = null;
+            try {
+                spiceClass = (Class<? extends Spice>) Class.forName("rocks.zipcode.quiz4.collections.culonary." + spice.getName());
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+            if (spiceMap.keySet().contains(spiceClass)) {
+                spiceMap.put(spiceClass, spiceMap.get(spiceClass) + 1);
+            } else {
+                spiceMap.put(spiceClass, 1);
+            }
+        }
+
+        return (Map<SpiceType, Integer>) spiceMap;
     }
 
     public void applySpice(Spice spice) {
+        spicesApplied.add(spice);
     }
 }
