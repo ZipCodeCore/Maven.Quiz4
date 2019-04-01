fbb3c9f294e8bf1070628d43babb9cff0891ff55
Marshilla Brahma <marshillabrahma@zipcoders-MacBook-Pro-10.local>
2019-04-01 11:16:37 -0400

    Completed object oriented section

--- src/main/java/rocks/zipcode/quiz4/objectorientation/account/Bank.java
+++ src/main/java/rocks/zipcode/quiz4/objectorientation/account/Bank.java
@@ -1,17 +1,29 @@
 package rocks.zipcode.quiz4.objectorientation.account;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author leon on 27/12/2018.
  */
 public class Bank {
+
+    private List<BankAccount> accounts;
+
+    public Bank(){
+        accounts = new ArrayList<>();
+    }
     public BankAccount removeBankAccountByIndex(Integer indexNumber) {
-        return null;
+        BankAccount account = accounts.get(indexNumber);
+        accounts.remove(account);
+        return account;
     }
 
     public void addBankAccount(BankAccount bankAccount) {
+        accounts.add(bankAccount);
     }
 
     public Boolean containsBankAccount(BankAccount bankAccount) {
-        throw new UnsupportedOperationException("Method not yet implemented");
+        return accounts.contains(bankAccount);
     }
 }
--- src/main/java/rocks/zipcode/quiz4/objectorientation/account/BankAccount.java
+++ src/main/java/rocks/zipcode/quiz4/objectorientation/account/BankAccount.java
@@ -3,7 +3,38 @@ package rocks.zipcode.quiz4.objectorientation.account;
 /**
  * @author leon on 27/12/2018.
  */
-public class BankAccount {
-    public void setBalance(Double val) {
+public class BankAccount extends Account implements Transactable{
+    private Double balance;
+
+    public BankAccount(){
+        this.balance = 0.0;
     }
+    public void setBalance(Double val) {
+        this.balance = val;
+    }
+
+    @Override
+    public void deposit(Double amountToIncreaseBy) throws IllegalArgumentException{
+        if(amountToIncreaseBy>=0){
+            Double newValue = getBalance() + amountToIncreaseBy;
+            setBalance(newValue);
+        }else{
+            throw new IllegalArgumentException();
+        }
+    }
+
+    @Override
+    public void withdrawal(Double amountToDecreaseBy) throws IllegalArgumentException{
+        if(amountToDecreaseBy>=0 && balance>=amountToDecreaseBy){
+            Double newValue = getBalance() - amountToDecreaseBy;
+            setBalance(newValue);
+        }else{
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public Double getBalance(){
+        return balance;
+    }
+
 }
