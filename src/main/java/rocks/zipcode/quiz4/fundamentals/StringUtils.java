43ea47a37cf90839d1c67de4f083dc561b7e54e5
Cristina McClintock <cristinamcclintock@zipcoders-MacBook-Pro.local>
2019-04-01 11:15:16 -0400

    checking in but not done

--- src/main/java/rocks/zipcode/quiz4/fundamentals/StringUtils.java
+++ src/main/java/rocks/zipcode/quiz4/fundamentals/StringUtils.java
@@ -5,30 +5,93 @@ package rocks.zipcode.quiz4.fundamentals;
  */
 public class StringUtils {
     public static Character getMiddleCharacter(String string) {
-        return null;
+        int mid = string.length()/2;
+        return string.charAt(mid);
     }
 
     public static String capitalizeMiddleCharacter(String str) {
-        return null;
+        String[] array = str.split("");
+        String answer = "";
+        for(int i = 0; i < array.length; i++){
+            if(i == str.length()/2){
+                answer+= getMiddleCharacter(str).toString().toUpperCase();
+            }else{
+                answer+= str.charAt(i);
+            }
+        }
+        return answer;
     }
 
     public static String lowerCaseMiddleCharacter(String str) {
-        return null;
+        String[] array = str.split("");
+        String answer = "";
+        for(int i = 0; i < array.length; i++){
+            if(i == str.length()/2){
+                answer+= getMiddleCharacter(str).toString().toLowerCase();
+            }else{
+                answer+= str.charAt(i);
+            }
+        }
+        return answer;
     }
 
     public static Boolean isIsogram(String str) {
-        return null;
+
+        for(int i = 0; i< str.length(); i++){
+            String sub = str.substring(i+1);
+            char c = str.charAt(i);
+            if(sub.indexOf(c) < 0){
+                return true;
+            }
+
+        }
+        return false;
     }
 
     public static Boolean hasDuplicateConsecutiveCharacters(String str) {
-        return null;
+        String[] stringArray = str.split("");
+        Boolean bool = false;
+        String prev = stringArray[0];
+        String answer = stringArray[0];
+
+        for(int i =1; i < stringArray.length; i++){
+            if(stringArray[i].equals(prev)){
+             return true;
+            } else {
+                prev = stringArray[i];
+            }
+        }
+        return bool;
+
     }
 
     public static String removeConsecutiveDuplicateCharacters(String str) {
-        return null;
+        String[] stringArray = str.split("");
+
+        String prev = stringArray[0];
+        String answer = stringArray[0];
+
+        for(int i =1; i < stringArray.length; i++){
+            if(!stringArray[i].equals(prev)){
+                answer+=stringArray[i];
+                prev = stringArray[i];
+            } else {
+                prev = stringArray[i];
+            }
+        }
+        return answer;
     }
 
     public static String invertCasing(String str) {
-        return null;
+        String answer = "";
+
+        for(int i = 0; i < str.length(); i++){
+            if(Character.isLowerCase(str.charAt(i))){
+                answer+= Character.toUpperCase(str.charAt(i));
+            } else {
+                answer+= Character.toLowerCase(str.charAt(i));
+            }
+        }
+        return answer;
     }
 }
--- src/main/java/rocks/zipcode/quiz4/objectorientation/account/Bank.java
+++ src/main/java/rocks/zipcode/quiz4/objectorientation/account/Bank.java
@@ -1,17 +1,32 @@
 package rocks.zipcode.quiz4.objectorientation.account;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author leon on 27/12/2018.
  */
 public class Bank {
+
+    List<BankAccount> ba;
+
+    public Bank() {
+        this.ba  = new ArrayList();
+    }
+
     public BankAccount removeBankAccountByIndex(Integer indexNumber) {
-        return null;
+        BankAccount toBeRemoved = ba.get(indexNumber);
+        ba.remove(indexNumber.intValue());
+
+        return toBeRemoved;
     }
 
     public void addBankAccount(BankAccount bankAccount) {
+        ba.add(bankAccount);
     }
 
     public Boolean containsBankAccount(BankAccount bankAccount) {
-        throw new UnsupportedOperationException("Method not yet implemented");
+
+        return ba.contains(bankAccount);
     }
 }
