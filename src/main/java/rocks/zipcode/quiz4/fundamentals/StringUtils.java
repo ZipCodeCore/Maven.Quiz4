8c452c540655a405406fd0c6d78fb4180559f0e4
Nirmala <nirmala.talari@gmail.com>
2019-04-01 10:49:41 -0400

    StringsTestWWIP

--- src/main/java/rocks/zipcode/quiz4/fundamentals/StringUtils.java
+++ src/main/java/rocks/zipcode/quiz4/fundamentals/StringUtils.java
@@ -1,34 +1,96 @@
 package rocks.zipcode.quiz4.fundamentals;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 /**
  * @author leon on 21/12/2018.
  */
 public class StringUtils {
     public static Character getMiddleCharacter(String string) {
-        return null;
+        Character midChar;
+
+        midChar = string.charAt(string.length() / 2);
+
+//      midChar = string.charAt((string.length() / 2)+1);
+
+        return midChar;
     }
 
     public static String capitalizeMiddleCharacter(String str) {
-        return null;
+        Integer midIndex = str.length() / 2;
+        return str.substring(0, midIndex) + Character.toUpperCase(str.charAt(midIndex)) + str.substring(midIndex + 1);
     }
 
     public static String lowerCaseMiddleCharacter(String str) {
-        return null;
+        Integer midIndex = str.length() / 2;
+
+        return str.substring(0, midIndex) + Character.toLowerCase(str.charAt(midIndex)) + str.substring(midIndex + 1);
     }
 
     public static Boolean isIsogram(String str) {
-        return null;
+        char[] charArr = str.toCharArray();
+        Arrays.sort(charArr);
+
+        for (int i = 0; i <str.length()-1 ; i++) {
+            if(charArr[i] == charArr[i+1])
+                return false;
+        }
+
+        return true;
     }
 
     public static Boolean hasDuplicateConsecutiveCharacters(String str) {
-        return null;
+        Boolean flag = false;
+
+        for (int i = str.length() - 1; i > 0; i--) {
+            if (str.charAt(i) == str.charAt(i - 1)) {
+                flag = true;
+            }
+        }
+
+        return flag;
     }
 
     public static String removeConsecutiveDuplicateCharacters(String str) {
-        return null;
+        StringBuilder builder = new StringBuilder();
+
+        if(hasDuplicateConsecutiveCharacters(str)) {
+           int index;
+
+            for (int i = 0; i <str.length() ; i++) {
+                char c = str.charAt(i);
+                index = str.indexOf(c,i+1);
+                if(index == -1){
+                    builder.append(c);
+                }
+            }
+
+           // str.chars().distinct().forEach(x -> builder.append((char) x));
+        }
+        else{
+            return str;
+        }
+
+        return builder.toString();
     }
 
     public static String invertCasing(String str) {
-        return null;
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isLetter(str.charAt(i)) && Character.isLowerCase(str.charAt(i))) {
+                builder.append(Character.toUpperCase(str.charAt(i)));
+            } else if (Character.isLetter(str.charAt(i)) && Character.isUpperCase(str.charAt(i))) {
+                builder.append(Character.toLowerCase(str.charAt(i)));
+            } else {
+                builder.append(str.charAt(i));
+            }
+
+        }
+
+        return builder.toString();
     }
 }
