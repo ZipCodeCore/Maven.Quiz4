a5fbda932efefb445a05df83e046f3b67ac1ae8b
Julian Rios <julianrios@zipcoders-MacBook-Pro-3.local>
2019-04-01 10:46:44 -0400

    passed all tests for fundamentals and arrays

--- src/main/java/rocks/zipcode/quiz4/arrays/ArrayUtils.java
+++ src/main/java/rocks/zipcode/quiz4/arrays/ArrayUtils.java
@@ -1,22 +1,27 @@
 package rocks.zipcode.quiz4.arrays;
 
+import java.util.Arrays;
+
 /**
  * @author leon on 01/01/2019.
  */
 public class ArrayUtils {
     public static String getMiddleElement(String[] values) {
-        return null;
+        return values[values.length/2];
     }
 
     public static String[] removeMiddleElement(String[] values) {
-        return null;
+        for (int i = values.length/2; i < values.length-1; i++) {
+            values[i] = values[i+1];
+        }
+        return Arrays.copyOf(values, values.length-1);
     }
 
     public static String getLastElement(String[] values) {
-        return null;
+        return values[values.length-1];
     }
 
     public static String[] removeLastElement(String[] values) {
-        return null;
+        return Arrays.copyOf(values, values.length-1);
     }
 }
