90a6547515a2ac9623e85aeaa8f58d811efaa563
zipcodeFrancisco <zipcodefrancisco@gmail.com>
2019-04-01 10:33:28 -0400

    Quiz4 -  Calculator part and StringUtils completed. 100% test has been passed.

--- src/main/java/rocks/zipcode/quiz4/fundamentals/StringUtils.java
+++ src/main/java/rocks/zipcode/quiz4/fundamentals/StringUtils.java
@@ -1,34 +1,82 @@
 package rocks.zipcode.quiz4.fundamentals;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 /**
  * @author leon on 21/12/2018.
  */
 public class StringUtils {
     public static Character getMiddleCharacter(String string) {
-        return null;
+        int lenghtOfString = string.length();
+        return string.charAt(lenghtOfString / 2);
     }
 
     public static String capitalizeMiddleCharacter(String str) {
-        return null;
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(str);
+        Integer indexOfMiddle = str.length() / 2;
+        stringBuilder.replace( indexOfMiddle,indexOfMiddle+1,
+                String.valueOf(stringBuilder.charAt(indexOfMiddle)).toUpperCase());
+
+        return stringBuilder.toString();
     }
 
     public static String lowerCaseMiddleCharacter(String str) {
-        return null;
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(str);
+        Integer indexOfMiddle = str.length() / 2;
+        stringBuilder.replace( indexOfMiddle,indexOfMiddle+1,
+                String.valueOf(stringBuilder.charAt(indexOfMiddle)).toLowerCase());
+
+        return stringBuilder.toString();
     }
 
     public static Boolean isIsogram(String str) {
-        return null;
+        Map<String, Long> x = Arrays.stream(str.split(""))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        for (Long l : x.values()) {
+            if (l > 1){
+                return false;
+            }
+        }
+        return true;
     }
 
     public static Boolean hasDuplicateConsecutiveCharacters(String str) {
-        return null;
+        for (int i = 0; i < str.length() - 2; i++) {
+            if (str.substring(i, i + 1).equals(str.substring(i + 1, i + 2)) ){
+                return true;
+            }
+        }
+        return false;
     }
 
     public static String removeConsecutiveDuplicateCharacters(String str) {
-        return null;
+        StringBuilder strBui = new StringBuilder();
+        strBui.append(str);
+        int index = 0;
+        for (int i = 0; i < strBui.length()-2; i++) {
+            if (strBui.charAt(i) == strBui.charAt(i + 1)){
+                strBui.delete(i, i + 2);
+                index++;
+            }
+        }
+        return strBui.toString();
     }
 
     public static String invertCasing(String str) {
-        return null;
+        StringBuilder strBui = new StringBuilder();
+        Character[] strArray = Arrays.stream(str.split(""))
+                .map(s -> s.charAt(0))
+                .toArray(Character[]::new);
+        for (Character c : strArray) {
+            strBui.append( Character.isUpperCase(c)
+                    ? c.toString().toLowerCase()
+                    : c.toString().toUpperCase() );
+        }
+        return strBui.toString();
     }
 }
