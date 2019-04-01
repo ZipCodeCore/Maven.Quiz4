e1e305fc9c646318ec2f54ce69ce9b1ccf0bad2c
Mark Moll <mdmoll@gmail.com>
2019-04-01 12:37:55 -0400

    75 percent of all tests passing

--- src/main/java/rocks/zipcode/quiz4/objectorientation/account/BankAccount.java
+++ src/main/java/rocks/zipcode/quiz4/objectorientation/account/BankAccount.java
@@ -3,7 +3,28 @@ package rocks.zipcode.quiz4.objectorientation.account;
 /**
  * @author leon on 27/12/2018.
  */
-public class BankAccount {
+public class BankAccount extends Account implements Transactable {
+    private Double balance;
+    
     public void setBalance(Double val) {
+        balance = val;
+    }
+    
+    public void deposit(Double amountToIncreaseBy) {
+        if (amountToIncreaseBy < 0) {
+            throw new IllegalArgumentException("Can't deposit negative amounts");
+        }
+        balance = balance + amountToIncreaseBy;
+    }
+    
+    public void withdrawal(Double amountToDecreaseBy) {
+        if (amountToDecreaseBy < 0) {
+            throw new IllegalArgumentException("Can't withdraw negative amounts");
+        }
+        balance = balance - amountToDecreaseBy;
+    }
+    
+    public Double getBalance() {
+        return balance;
     }
 }
