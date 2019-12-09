d951fef20eba9e518088ad72edfd0e7f4f11da9a
Ron Duwell <ronduwell@zipcoders-MacBook-Pro-4.local>
2019-12-09 18:29:17 -0500

    pushing just in case

--- src/main/java/rocks/zipcode/quiz4/arrays/ArrayUtils.java
+++ src/main/java/rocks/zipcode/quiz4/arrays/ArrayUtils.java
@@ -1,22 +1,43 @@
 package rocks.zipcode.quiz4.arrays;
 
+import java.util.ArrayList;
+
 /**
  * @author leon on 01/01/2019.
  */
 public class ArrayUtils {
     public static String getMiddleElement(String[] values) {
-        return null;
+        return values[values.length/2];
     }
 
     public static String[] removeMiddleElement(String[] values) {
-        return null;
+        ArrayList<String> newList = new ArrayList<>();
+        for(int i = 0; i < values.length; i++){
+            if(i != values.length / 2){
+                newList.add(values[i]);
+            }
+        }
+        return newList.toArray(new String[0]);
+
+        /*String[] newArray = new String[values.length - 1];
+        for(int i = 0; i < values.length / 2; i++){
+            newArray[i] = values[i];
+        }
+        for(int i = (values.length / 2) + 1; i < values.length; i++){
+            newArray[i] = values[i + 1];
+        }
+        return newArray;*/
     }
 
     public static String getLastElement(String[] values) {
-        return null;
+        return values[values.length - 1];
     }
 
     public static String[] removeLastElement(String[] values) {
-        return null;
+        String[] newArray = new String[values.length - 1];
+        for(int i = 0; i < values.length - 1; i++){
+            newArray[i] = values[i];
+        }
+        return newArray;
     }
 }
