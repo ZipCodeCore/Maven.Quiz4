abac05af9471262e99f4d9832170977491fe2a02
jeanvalentin51 <valentin.j.gheorghe.com>
2019-12-09 18:46:58 -0500

    account polymorphism

--- src/main/java/rocks/zipcode/quiz4/collections/culonary/Food.java
+++ src/main/java/rocks/zipcode/quiz4/collections/culonary/Food.java
@@ -1,6 +1,7 @@
 package rocks.zipcode.quiz4.collections.culonary;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -17,6 +18,7 @@ public class Food {
     }
 
     public <SpiceType extends Class<? extends Spice>> Map<SpiceType, Integer> getSpiceCount() {
+
         return null;
     }
 
