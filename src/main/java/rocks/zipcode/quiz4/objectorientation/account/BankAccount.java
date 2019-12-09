c42de8b9034c14bcb0c621900eec6807a9da6148
jeanvalentin51 <valentin.j.gheorghe.com>
2019-12-09 18:50:47 -0500

    withdraw positive

--- src/main/java/rocks/zipcode/quiz4/objectorientation/account/BankAccount.java
+++ src/main/java/rocks/zipcode/quiz4/objectorientation/account/BankAccount.java
@@ -5,21 +5,24 @@ package rocks.zipcode.quiz4.objectorientation.account;
  */
 public class BankAccount extends Account implements Transactable{
 
+    Double accountBalance;
+
     public void setBalance(Double val) {
+        this.accountBalance =val;
     }
 
     @Override
     public void deposit(Double amountToIncreaseBy) {
-
+        accountBalance =+ amountToIncreaseBy;
     }
 
     @Override
     public void withdrawal(Double amountToDecreaseBy) {
-
+        accountBalance -= amountToDecreaseBy;
     }
 
     @Override
     public Double getBalance() {
-        return null;
+        return this.accountBalance;
     }
 }
