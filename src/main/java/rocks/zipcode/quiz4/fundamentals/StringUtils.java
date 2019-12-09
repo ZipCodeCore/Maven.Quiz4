203093c30ae2c5379fee05787a09df7d30b851af
jeanvalentin51 <valentin.j.gheorghe.com>
2019-12-09 17:56:22 -0500

    capitalize middle

--- src/main/java/rocks/zipcode/quiz4/fundamentals/StringUtils.java
+++ src/main/java/rocks/zipcode/quiz4/fundamentals/StringUtils.java
@@ -4,13 +4,24 @@ package rocks.zipcode.quiz4.fundamentals;
  * @author leon on 21/12/2018.
  */
 public class StringUtils {
+
     public static Character getMiddleCharacter(String string) {
         int middle = string.length() / 2;
         return string.charAt(middle);
     }
 
     public static String capitalizeMiddleCharacter(String str) {
-        return null;
+        String result = "";
+
+        for (int i = 0; i < str.length(); i++){
+            if (i == str.length()/2) {
+                result = result.concat(String.valueOf(str.toUpperCase().charAt(i)));
+            } else {
+                result = result.concat(String.valueOf(str.charAt(i)));
+            }
+        }
+
+        return result;
     }
 
     public static String lowerCaseMiddleCharacter(String str) {
