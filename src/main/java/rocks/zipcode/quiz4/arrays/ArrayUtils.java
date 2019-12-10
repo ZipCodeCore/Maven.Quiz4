e029f0173349cb8093f80e1d56c0dc7213ce6ba8
kshields412 <kshields412@gmail.com>
2019-12-09 19:19:15 -0500

    checkpoint

--- src/main/java/rocks/zipcode/quiz4/arrays/ArrayUtils.java
+++ src/main/java/rocks/zipcode/quiz4/arrays/ArrayUtils.java
@@ -1,22 +1,45 @@
 package rocks.zipcode.quiz4.arrays;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author leon on 01/01/2019.
  */
 public class ArrayUtils {
     public static String getMiddleElement(String[] values) {
-        return null;
+        Integer size = values.length;
+        Integer index = size/2;
+        return values[index];
     }
 
     public static String[] removeMiddleElement(String[] values) {
-        return null;
+        ArrayList<String> result = new ArrayList<>();
+        for (String s : values) {
+            if (!values.equals(getMiddleElement(values))) {
+                result.add(s);
+            }
+        }
+        String[] output = new String[result.size()];
+        output = result.toArray(output);
+        return output;
     }
 
     public static String getLastElement(String[] values) {
-        return null;
+        Integer size = values.length;
+        return values[size-1];
     }
 
     public static String[] removeLastElement(String[] values) {
-        return null;
+        ArrayList<String> result = new ArrayList<>();
+        for (String s : values) {
+            if (!s.equals(getLastElement(values))) {
+                result.add(s);
+            }
+        }
+        String[] output = new String[result.size()];
+        output = result.toArray(output);
+        return output;
     }
 }
--- src/main/java/rocks/zipcode/quiz4/fundamentals/StringUtils.java
+++ src/main/java/rocks/zipcode/quiz4/fundamentals/StringUtils.java
@@ -1,34 +1,82 @@
 package rocks.zipcode.quiz4.fundamentals;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * @author leon on 21/12/2018.
  */
 public class StringUtils {
     public static Character getMiddleCharacter(String string) {
-        return null;
+        Integer index = string.length() / 2;
+        return string.charAt(index);
     }
 
     public static String capitalizeMiddleCharacter(String str) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            if (i == (str.length() / 2)) {
+                sb.append(str.toUpperCase().charAt(i));
+            } else {
+                sb.append(str.charAt(i));
+            }
+        }
+        return sb.toString();
     }
 
     public static String lowerCaseMiddleCharacter(String str) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            if (i == (str.length() / 2)) {
+                sb.append(str.toLowerCase().charAt(i));
+            } else {
+                sb.append(str.charAt(i));
+            }
+        }
+        return sb.toString();
     }
 
     public static Boolean isIsogram(String str) {
-        return null;
+        str = str.toLowerCase();
+        int length = str.length();
+
+        char arr[] = str.toCharArray();
+
+        Arrays.sort(arr);
+        for (int i = 0; i < length; i++) {
+            if (arr[i] == arr[i + 1]) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static Boolean hasDuplicateConsecutiveCharacters(String str) {
-        return null;
+        char[] newStr = str.toCharArray();
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (newStr[i] == newStr[i + 1]) {
+                return true;
+            }
+        }
+        return false;
+
     }
 
     public static String removeConsecutiveDuplicateCharacters(String str) {
         return null;
     }
 
+
     public static String invertCasing(String str) {
-        return null;
+        char[] newArr = str.toCharArray();
+        for (int i = 0; i < newArr.length; i++) {
+            char c = newArr[i];
+            if (Character.isUpperCase(c)) {
+                newArr[i] = Character.toLowerCase(c);
+            } else if (Character.isLowerCase(c)) {
+                newArr[i] = Character.toUpperCase(c);
+            }
+        }
+        return new String(newArr);
     }
 }
