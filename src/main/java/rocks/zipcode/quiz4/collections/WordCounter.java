573ca72928e29a1aa6862c31394202c30e538767
David Trombello <david@zipcoders-MacBook-Pro-96.local>
2019-12-10 11:18:40 -0500

    more tests pass

--- src/main/java/rocks/zipcode/quiz4/collections/WordCounter.java
+++ src/main/java/rocks/zipcode/quiz4/collections/WordCounter.java
@@ -1,14 +1,27 @@
 package rocks.zipcode.quiz4.collections;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class WordCounter {
-    String [] strings;
+    //String [] strings;
+    Map<String, Long> wordCounts = new HashMap<>();
+    Map<String, Integer> newMap = new HashMap<>();
 
     public WordCounter(String... strings) {
+        wordCounts =
+                Arrays.stream(strings).collect(Collectors.groupingBy(w -> w, Collectors.counting()));
+        newMap = wordCounts.entrySet()
+                .stream()
+                .collect(Collectors.toMap(entry -> entry.getKey(), entry -> entry.getValue().intValue()));
     }
 
-    public Map<String, Integer> getWordCountMap() {
-        return null;
+    //Write method for using a loop also.
+
+    public Map<String, Integer> getWordCountMap(String... strings) {
+        return newMap;
     }
 }
