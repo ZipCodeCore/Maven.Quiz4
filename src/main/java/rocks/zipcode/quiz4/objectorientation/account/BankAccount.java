d76e47628956ae31d4bd1adad1ac6c365864da08
wes <wes.jones1597@gmail.com>
2019-12-09 21:05:48 -0500

    137/154

--- src/main/java/rocks/zipcode/quiz4/objectorientation/account/BankAccount.java
+++ src/main/java/rocks/zipcode/quiz4/objectorientation/account/BankAccount.java
@@ -3,7 +3,32 @@ package rocks.zipcode.quiz4.objectorientation.account;
 /**
  * @author leon on 27/12/2018.
  */
-public class BankAccount {
+public class BankAccount extends Account implements Transactable {
+    private Double balance;
+
+    public BankAccount() {
+        this.balance = 0.00;
+    }
+
     public void setBalance(Double val) {
+        this.balance = val;
+    }
+
+    public void deposit(Double val) {
+        if (val > 0) this.setBalance(this.getBalance()+val);
+        else throw new IllegalArgumentException();
+    }
+
+    @Override
+    public void withdrawal(Double amountToDecreaseBy) {
+        if (amountToDecreaseBy < 0 || this.getBalance() < amountToDecreaseBy)
+            throw new IllegalArgumentException();
+        else
+            this.setBalance(this.getBalance()-amountToDecreaseBy);
+    }
+
+    @Override
+    public Double getBalance() {
+        return this.balance;
     }
 }
