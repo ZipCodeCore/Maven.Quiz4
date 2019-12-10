5555255dced517629cfcd18459e5d61e6a56f97f
Amendoza20 <20.angel.mendoza@gmail.com>
2019-12-09 20:17:35 -0500

    do it for the Push

--- src/main/java/rocks/zipcode/quiz4/arrays/ArrayUtils.java
+++ src/main/java/rocks/zipcode/quiz4/arrays/ArrayUtils.java
@@ -1,22 +1,30 @@
 package rocks.zipcode.quiz4.arrays;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author leon on 01/01/2019.
  */
 public class ArrayUtils {
     public static String getMiddleElement(String[] values) {
-        return null;
+        return values[values.length / 2];
     }
 
     public static String[] removeMiddleElement(String[] values) {
-        return null;
+        List<String> newArray = new ArrayList<>(Arrays.asList(values));
+        newArray.remove(getMiddleElement(values));
+        return newArray.toArray(new String[0]);
     }
 
     public static String getLastElement(String[] values) {
-        return null;
+        return values[values.length - 1];
     }
 
     public static String[] removeLastElement(String[] values) {
-        return null;
+        List<String> newArray = new ArrayList<>(Arrays.asList(values));
+        newArray.remove(getLastElement(values));
+        return newArray.toArray(new String[0]);
     }
 }
