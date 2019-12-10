1a4334c422e53d438c30248b1d3ad78c8b1f8604
SandyPham823 <sandydupham@gmail.com>
2019-12-09 23:28:22 -0500

    finished array utils

--- src/main/java/rocks/zipcode/quiz4/arrays/ArrayUtils.java
+++ src/main/java/rocks/zipcode/quiz4/arrays/ArrayUtils.java
@@ -1,22 +1,29 @@
 package rocks.zipcode.quiz4.arrays;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author leon on 01/01/2019.
  */
 public class ArrayUtils {
     public static String getMiddleElement(String[] values) {
-        return null;
+        return values[values.length / 2];
     }
 
     public static String[] removeMiddleElement(String[] values) {
-        return null;
+        List<String> l = new ArrayList<>(Arrays.asList(Arrays.copyOfRange(values, 0, values.length / 2)));
+        l.addAll(Arrays.asList(Arrays.copyOfRange(values, values.length/2+1, values.length)));
+
+        return l.toArray(new String[l.size()]);
     }
 
     public static String getLastElement(String[] values) {
-        return null;
+        return values[values.length - 1];
     }
 
     public static String[] removeLastElement(String[] values) {
-        return null;
+        return Arrays.copyOfRange(values,0,values.length - 1);
     }
 }
