ae805e692f5637289b0dd4b4c90dbb26449e4d8a
ushanil <ush.gopi@gmail.com>
2019-12-10 05:42:46 -0500

    20 passed

--- src/main/java/rocks/zipcode/quiz4/collections/WordCounter.java
+++ src/main/java/rocks/zipcode/quiz4/collections/WordCounter.java
@@ -1,12 +1,23 @@
 package rocks.zipcode.quiz4.collections;
 
-import java.util.Map;
+import java.util.*;
 
 public class WordCounter {
+    private List<String> words;
     public WordCounter(String... strings) {
+        words = Arrays.asList(strings);
+    }
+    public WordCounter() {
+        words =  new ArrayList<>();
     }
 
+
     public Map<String, Integer> getWordCountMap() {
-        return null;
+
+        Map<String, Integer> frequencyMap = new HashMap<>();
+        for(String word : words) {
+            frequencyMap.put(word, Collections.frequency(words, word));
+        }
+        return frequencyMap;
     }
 }
