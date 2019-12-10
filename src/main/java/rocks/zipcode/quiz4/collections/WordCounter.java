2128543fb74e61ffa532f1e908e1a216d6c1a3f1
jeanvalentin51 <valentin.j.gheorghe.com>
2019-12-09 22:33:37 -0500

    Add word counter

--- src/main/java/rocks/zipcode/quiz4/collections/WordCounter.java
+++ src/main/java/rocks/zipcode/quiz4/collections/WordCounter.java
@@ -8,13 +8,22 @@ import java.util.Map;
 public class WordCounter {
 
     Map<String,Integer> words = new HashMap<>();
+    Integer counter;
 
     public WordCounter(String... strings) {
+
+        for (String each : strings){
+            counter = words.get(each);
+            if (counter == null){
+                counter = 1;
+            } else counter ++;
+
+            words.put(each,counter);
+        }
 
     }
 
     public Map<String, Integer> getWordCountMap() {
-
-        return null;
+        return words;
     }
 }
