8fd250a003de8bb0598a12158583c157606326bb
Amendoza20 <20.angel.mendoza@gmail.com>
2019-12-09 20:28:40 -0500

    Push the Spice for Roberto

--- src/main/java/rocks/zipcode/quiz4/collections/WordCounter.java
+++ src/main/java/rocks/zipcode/quiz4/collections/WordCounter.java
@@ -1,11 +1,21 @@
 package rocks.zipcode.quiz4.collections;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class WordCounter {
+    Map<String, Integer> spiceMap;
     public WordCounter(String... strings) {
-    }
+        spiceMap = new HashMap<>();
+        for (String string : strings) {
+            if (spiceMap.containsKey(string)) {
+                spiceMap.replace(string, spiceMap.get(string) + 1);
+            }else{
+                spiceMap.put(string, 1);
+            }
 
+        }
+    }
     public Map<String, Integer> getWordCountMap() {
         return null;
     }
