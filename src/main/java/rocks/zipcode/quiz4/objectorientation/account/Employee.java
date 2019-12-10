4be4d10c1dd566252e8aba9ea744aff5228e6121
Amendoza20 <20.angel.mendoza@gmail.com>
2019-12-09 21:17:22 -0500

    push push push

--- src/main/java/rocks/zipcode/quiz4/objectorientation/account/Employee.java
+++ src/main/java/rocks/zipcode/quiz4/objectorientation/account/Employee.java
@@ -3,11 +3,17 @@ package rocks.zipcode.quiz4.objectorientation.account;
 /**
  * @author leon on 30/12/2018.
  */
-public class Employee {
+public class Employee implements Worker, Transactable{
+    private BankAccount bankAccount;
+    private Double hoursWorked;
+    private Double hourlyWage;
     public Employee() {
+        hoursWorked =0.0;
+        hourlyWage = 35.0;
     }
 
     public Employee(BankAccount bankAccount) {
+        this.bankAccount = bankAccount;
     }
 
     public BankAccount getBankAccount() {
@@ -15,6 +21,49 @@ public class Employee {
     }
 
     public void setBankAccount(BankAccount bankAccount) {
+        this.bankAccount = bankAccount;
+    }
 
+    public void setHoursWorked(Double hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+
+    public void setHourlyWage(Double hourlyWage) {
+        this.hourlyWage = hourlyWage;
+    }
+
+    @Override
+    public void increaseHoursWorked(Double numberOfHours) {
+        hoursWorked += numberOfHours;
+    }
+
+    @Override
+    public Double getHoursWorked() {
+        return hoursWorked;
+    }
+
+    @Override
+    public Double getHourlyWage() {
+        return hourlyWage;
+    }
+
+    @Override
+    public Double getMoneyEarned() {
+        return hourlyWage * hoursWorked;
+    }
+
+    @Override
+    public void deposit(Double amountToIncreaseBy) {
+        bankAccount.deposit(amountToIncreaseBy);
+    }
+
+    @Override
+    public void withdrawal(Double amountToDecreaseBy) {
+        bankAccount.withdrawal(amountToDecreaseBy);
+    }
+
+    @Override
+    public Double getBalance() {
+        return bankAccount.getBalance();
     }
 }
