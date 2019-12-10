edfc3498e8e921e5f08c7b893d997e508179a0d8
ushanil <ush.gopi@gmail.com>
2019-12-10 05:46:25 -0500

    55 passed

--- src/main/java/rocks/zipcode/quiz4/fundamentals/Calculator.java
+++ src/main/java/rocks/zipcode/quiz4/fundamentals/Calculator.java
@@ -1,35 +1,42 @@
 package rocks.zipcode.quiz4.fundamentals;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * @author leon on 21/12/2018.
  */
 public class Calculator {
     public static Double squareRoot(Double value) {
-        return null;
+        return Math.sqrt(value);
     }
 
     public static Double square(Double value) {
-        return null;
+        return value*value;
     }
 
     public static Double[] squareRoots(Double... value) {
-        return null;
-    }
+        List<Double> squareRoots =  Arrays.stream(value).map(number -> Math.sqrt(number))
+                .collect(Collectors.toList());
+        return squareRoots.toArray(new Double[squareRoots.size()]);}
 
     public static Double[] squares(Double... values) {
-        return null;
+        List<Double> squareRoots =  Arrays.stream(values).map(number -> number * number)
+                .collect(Collectors.toList());
+        return squareRoots.toArray(new Double[squareRoots.size()]);
     }
 
     public static Double add(Double value1, Double value2) {
-        return null;
+        return value1+value2;
     }
 
     public static Double subtract(Double value1, Double value2) {
-        return null;
+        return value1-value2;
     }
 
 
     public static Double divide(Double divisor, Double dividend) {
-        return null;
+        return divisor/dividend;
     }
 }
