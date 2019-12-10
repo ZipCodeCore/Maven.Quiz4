17d10fbad1563b2a40ca7fcdd5768f4ca522c675
SandyPham823 <sandydupham@gmail.com>
2019-12-10 00:00:40 -0500

    finished culonary portion

--- src/main/java/rocks/zipcode/quiz4/collections/culonary/Food.java
+++ src/main/java/rocks/zipcode/quiz4/collections/culonary/Food.java
@@ -1,5 +1,7 @@
 package rocks.zipcode.quiz4.collections.culonary;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -7,14 +9,21 @@ import java.util.Map;
  * @author leon on 27/12/2018.
  */
 public class Food {
+    private Map<Class<? extends Spice>, Integer> spiceCount = new LinkedHashMap<>();
+    private List<Spice> spices = new ArrayList<>();
+
     public List<Spice> getAllSpices() {
-        return null;
+        return spices;
     }
 
-    public <SpiceType extends Class<? extends Spice>> Map<SpiceType, Integer> getSpiceCount() {
-        return null;
+    public Map<Class<? extends Spice>, Integer> getSpiceCount() {
+        return spiceCount;
     }
 
     public void applySpice(Spice spice) {
+        spiceCount.putIfAbsent(spice.getClass(), 0);
+        spiceCount.replace(spice.getClass(),spiceCount.get(spice.getClass()) + 1);
+
+        spices.add(spice);
     }
 }
