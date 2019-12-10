56385a56e0ee84d560ec1f31b62016fe9d69d944
SandyPham823 <sandydupham@gmail.com>
2019-12-09 23:20:02 -0500

    finished all of fundamentals

--- src/main/java/rocks/zipcode/quiz4/fundamentals/StringUtils.java
+++ src/main/java/rocks/zipcode/quiz4/fundamentals/StringUtils.java
@@ -5,30 +5,57 @@ package rocks.zipcode.quiz4.fundamentals;
  */
 public class StringUtils {
     public static Character getMiddleCharacter(String string) {
-        return null;
+        return string.charAt(string.length() / 2);
     }
 
     public static String capitalizeMiddleCharacter(String str) {
-        return null;
+        return str.substring(0, str.length() / 2) + Character.toUpperCase(getMiddleCharacter(str)) + str.substring(str.length() / 2 + 1);
     }
 
     public static String lowerCaseMiddleCharacter(String str) {
-        return null;
+        return str.substring(0, str.length() / 2) + Character.toLowerCase(getMiddleCharacter(str)) + str.substring(str.length() / 2 + 1);
     }
 
     public static Boolean isIsogram(String str) {
-        return null;
+        for (char c : str.toCharArray())
+            if (!(str.chars().filter(ch -> ch == c).count() == 1))
+                return false;
+
+            return true;
     }
 
     public static Boolean hasDuplicateConsecutiveCharacters(String str) {
-        return null;
+        for (int i = 0; i < str.length() - 1; i++)
+            if (str.charAt(i) == str.charAt(i + 1))
+                return true;
+
+        return false;
     }
 
     public static String removeConsecutiveDuplicateCharacters(String str) {
-        return null;
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < str.length()-1; i++)
+            if (str.charAt(i)==str.charAt(i+1))
+                i++;
+            else
+                result.append(str.charAt(i));
+
+        result.append(str.charAt(str.length()-1));
+
+        return result.toString();
     }
 
     public static String invertCasing(String str) {
-        return null;
+        StringBuilder result = new StringBuilder();
+        for (char c : str.toCharArray())
+            if (Character.isLowerCase(c))
+                result.append(Character.toUpperCase(c));
+            else if (Character.isUpperCase(c))
+                result.append(Character.toLowerCase(c));
+            else
+                result.append(c);
+
+        return result.toString();
     }
 }
