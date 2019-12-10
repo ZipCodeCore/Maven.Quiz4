99fa7b27c6aa6003b38c4e5786250c46239b6f51
mike <mike.schoenbeck@gmail.com>
2019-12-10 01:04:35 -0500

    started collections

--- src/main/java/rocks/zipcode/quiz4/collections/WordCounter.java
+++ src/main/java/rocks/zipcode/quiz4/collections/WordCounter.java
@@ -1,12 +1,34 @@
 package rocks.zipcode.quiz4.collections;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class WordCounter {
+    private String[] strings;
+
     public WordCounter(String... strings) {
+        this.strings = strings;
     }
 
+
     public Map<String, Integer> getWordCountMap() {
-        return null;
+        int counter = 0;
+        int temp = 1;
+        HashMap<String, Integer> wordMap = new HashMap<>();
+        for (int i = 0; i < this.strings.length - 1; i++) {
+            for (int j = i + 1; j < strings.length; j++) {
+                if (wordMap.get(strings[j]).equals(null)) {
+                    wordMap.put(strings[j], 1);
+                }
+                else {
+                    temp = wordMap.get(strings[j]);
+                    temp++;
+                    wordMap.put(this.strings[i], temp);
+            }
+            }
+        }
+        return wordMap;
     }
 }
--- src/main/java/rocks/zipcode/quiz4/collections/culonary/Food.java
+++ src/main/java/rocks/zipcode/quiz4/collections/culonary/Food.java
@@ -6,7 +6,7 @@ import java.util.Map;
 /**
  * @author leon on 27/12/2018.
  */
-public class Food {
+public class Food <T extends Spice> {
     public List<Spice> getAllSpices() {
         return null;
     }
