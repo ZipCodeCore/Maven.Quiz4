5c0847f614dbb3c134ecb491ef894b4c1e913690
April Howard <ahoward@zipcodes-MBP-5.fios-router.home>
2020-03-20 16:25:44 -0400

    fixed hasDuplicateConsecutiveCharacters

--- src/main/java/rocks/zipcode/quiz4/fundamentals/StringUtils.java
+++ src/main/java/rocks/zipcode/quiz4/fundamentals/StringUtils.java
@@ -32,16 +32,16 @@ public class StringUtils {
 
     public static Boolean hasDuplicateConsecutiveCharacters(String str) {
         for (int i = 0; i < str.length() - 1; i++) {
-            for (int j = 0; j < str.length() - 1; j++) {
-                if (str.charAt(i) == str.charAt(j))
+                if (str.charAt(i) == str.charAt(i + 1))
                     return true;
             }
-        }
         return false;
     }
 
 
     public static String removeConsecutiveDuplicateCharacters(String str) {
+
+
         return null;
     }
 
