a98211fb059cfe8d87d8e1c5bf41fe5bf423f7c2
aartikansal <aartilumb@gmail.com>
2020-03-20 16:07:26 -0400

    squaresroots done

--- src/main/java/rocks/zipcode/quiz4/fundamentals/Calculator.java
+++ src/main/java/rocks/zipcode/quiz4/fundamentals/Calculator.java
@@ -9,17 +9,23 @@ public class Calculator {
     }
 
     public static Double square(Double value) {
-        return value*value;
+        return value * value;
     }
 
     public static Double[] squareRoots(Double... value) {
-        return null;
+        Double[] result = new Double[value.length];
+        for (int i = 0; i < value.length; i++) {
+        result[i] = Math.sqrt(value[i]);
     }
-
+return result;
+}
     public static Double[] squares(Double... values) {
-        return null;
+        Double[] result = new Double[values.length];
+        for (int i = 0; i < values.length; i++) {
+            result[i] = values[i] * values[i];
+        }
+        return result;
     }
-
     public static Double add(Double value1, Double value2) {
         return value1 + value2;
     }
