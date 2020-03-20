7fedbc0d2e281e58c14395fc32682ab8f9a7b96b
asconem <asconem@gmail.com>
2020-03-20 14:27:01 -0400

    Saving progress through ArrayUtils

--- src/main/java/rocks/zipcode/quiz4/arrays/ArrayUtils.java
+++ src/main/java/rocks/zipcode/quiz4/arrays/ArrayUtils.java
@@ -1,22 +1,35 @@
 package rocks.zipcode.quiz4.arrays;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * @author leon on 01/01/2019.
  */
 public class ArrayUtils {
     public static String getMiddleElement(String[] values) {
-        return null;
+        return values[values.length /2];
     }
 
     public static String[] removeMiddleElement(String[] values) {
-        return null;
+
+        ArrayList<String> a = new ArrayList<>(Arrays.asList(values));
+        a.remove(getMiddleElement(values));
+        values = new String[a.size()];
+        a.toArray(values);
+        return values;
     }
 
     public static String getLastElement(String[] values) {
-        return null;
+
+        return values[values.length - 1];
     }
 
     public static String[] removeLastElement(String[] values) {
-        return null;
+        ArrayList<String> a = new ArrayList<>(Arrays.asList(values));
+        a.remove(getLastElement(values));
+        values = new String[a.size()];
+        a.toArray(values);
+        return values;
     }
 }
