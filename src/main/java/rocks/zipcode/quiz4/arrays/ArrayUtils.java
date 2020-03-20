de4050542cf9e7835b02b6e5ca98d89936422675
Moe Aydin <maydin@zipcodes-MacBook-Pro-23.local>
2020-03-20 15:32:35 -0400

    complete array utils

--- src/main/java/rocks/zipcode/quiz4/arrays/ArrayUtils.java
+++ src/main/java/rocks/zipcode/quiz4/arrays/ArrayUtils.java
@@ -5,18 +5,35 @@ package rocks.zipcode.quiz4.arrays;
  */
 public class ArrayUtils {
     public static String getMiddleElement(String[] values) {
-        return null;
+        return values[values.length / 2];
     }
 
     public static String[] removeMiddleElement(String[] values) {
-        return null;
+
+        String[] removeMiddle = new String[values.length - 1];
+        int mid = values.length / 2;
+        for (int i = 0; i < values.length ; i++) {
+            if(i < mid){
+                removeMiddle[i] = values[i];
+            }
+            if(i > mid) {
+                removeMiddle[i-1] = values[i];
+            }
+        }
+
+        return removeMiddle;
+
     }
 
     public static String getLastElement(String[] values) {
-        return null;
+        return values[values.length - 1];
     }
 
     public static String[] removeLastElement(String[] values) {
-        return null;
+
+        String[] removeLast = new String[values.length-1];
+        System.arraycopy(values, 0, removeLast, 0, values.length - 1);
+        return removeLast;
+
     }
 }
