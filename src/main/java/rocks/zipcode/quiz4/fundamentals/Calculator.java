608454c19c797df081706c7be43dabbc00cb7a0a
hlin <Zxsaqw12@&$>
2020-03-20 19:03:12 -0400

    First commit

--- src/main/java/rocks/zipcode/quiz4/fundamentals/Calculator.java
+++ src/main/java/rocks/zipcode/quiz4/fundamentals/Calculator.java
@@ -1,35 +1,46 @@
 package rocks.zipcode.quiz4.fundamentals;
 
+import java.util.stream.Stream;
+
 /**
  * @author leon on 21/12/2018.
  */
 public class Calculator {
     public static Double squareRoot(Double value) {
-        return null;
+        return Math.sqrt(value);
     }
 
     public static Double square(Double value) {
-        return null;
+        return value * value;
     }
 
     public static Double[] squareRoots(Double... value) {
-        return null;
+        Double[] result = (Double[]) Stream.of(value)
+                .map(root -> {
+                    double square = root * root;
+                    return square;
+                })
+                .toArray();
+        return result;
+
     }
 
     public static Double[] squares(Double... values) {
-        return null;
+        Double[] result = new Double[values.length];
+        Stream.of(result).forEach(Calculator::square);
+            return result;
     }
 
     public static Double add(Double value1, Double value2) {
-        return null;
+        return value1 + value2;
     }
 
     public static Double subtract(Double value1, Double value2) {
-        return null;
+        return value1 - value2;
     }
 
 
     public static Double divide(Double divisor, Double dividend) {
-        return null;
+        return divisor / dividend;
     }
 }
--- src/main/java/rocks/zipcode/quiz4/fundamentals/StringUtils.java
+++ src/main/java/rocks/zipcode/quiz4/fundamentals/StringUtils.java
@@ -1,34 +1,74 @@
 package rocks.zipcode.quiz4.fundamentals;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * @author leon on 21/12/2018.
  */
 public class StringUtils {
     public static Character getMiddleCharacter(String string) {
-        return null;
+        return string.charAt(string.length() / 2);
     }
 
     public static String capitalizeMiddleCharacter(String str) {
-        return null;
+        StringBuffer result = new StringBuffer(str);
+        for (int i = 0; i < str.length(); i++) {
+            int mid = str.length() / 2;
+            result.setCharAt(mid, Character.toUpperCase(str.charAt(mid)));
+        }
+        return result.toString();
     }
 
     public static String lowerCaseMiddleCharacter(String str) {
-        return null;
+        StringBuffer result = new StringBuffer(str);
+        for (int i = 0; i < str.length(); i++) {
+            int mid = str.length() / 2;
+            result.setCharAt(mid, Character.toLowerCase(str.charAt(mid)));
+        }
+        return result.toString();
     }
 
     public static Boolean isIsogram(String str) {
-        return null;
+        int length = str.length();
+        char[] arr = str.toCharArray();
+        Arrays.sort(arr);
+        for (int i = 0; i < length - 1; i++) {
+            if (arr[i] == arr[i + 1]) return false;
+        }
+        return true;
     }
 
     public static Boolean hasDuplicateConsecutiveCharacters(String str) {
-        return null;
+        return (str.matches(".*(.)\\1.*"));
     }
 
     public static String removeConsecutiveDuplicateCharacters(String str) {
-        return null;
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i) == str.charAt(i-1)) {
+                str = str.substring(0, i - 1) + str.substring(i + 1);
+                i = 0;
+            }
+        }
+        return str;
     }
 
     public static String invertCasing(String str) {
-        return null;
+        char[] array = str.toCharArray();
+
+        for (int i = 0; i < array.length; i++) {
+            char c = array[i];
+            if (Character.isUpperCase(c)) {
+                array[i] = Character.toLowerCase(c);
+            }
+            else if (Character.isLowerCase(c)) {
+                array[i] = Character.toUpperCase(c);
+            }
+        }
+        return new String(array);
     }
 }
