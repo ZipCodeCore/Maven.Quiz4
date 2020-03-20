6489fe55b624d2a51f1ab0073090fbde4a6d5826
April Howard <ahoward@zipcodes-MBP-5.fios-router.home>
2020-03-20 14:47:16 -0400

    arrays.arrayutils

--- src/main/java/rocks/zipcode/quiz4/arrays/ArrayUtils.java
+++ src/main/java/rocks/zipcode/quiz4/arrays/ArrayUtils.java
@@ -1,22 +1,32 @@
 package rocks.zipcode.quiz4.arrays;
 
+import java.util.Arrays;
+
 /**
  * @author leon on 01/01/2019.
  */
 public class ArrayUtils {
     public static String getMiddleElement(String[] values) {
-        return null;
+        return values[values.length/2];
     }
 
     public static String[] removeMiddleElement(String[] values) {
-        return null;
+        String[] result = new String [values.length-1];
+        int count = 0;
+        for(String element : values) {
+            if(!element.equals(getMiddleElement((values)))) {
+                result[count] = element;
+                count++;
+            }
+        }
+        return result;
     }
 
     public static String getLastElement(String[] values) {
-        return null;
+        return values[values.length - 1];
     }
 
     public static String[] removeLastElement(String[] values) {
-        return null;
+        return Arrays.copyOf(values, values.length-1);
     }
 }
