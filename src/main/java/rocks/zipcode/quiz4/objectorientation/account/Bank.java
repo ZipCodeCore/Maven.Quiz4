5a24a6cd621dc88054595e84e8c3eeb7f23a267f
Khalil Crumpler <crumplerkhalil@gmail.com>
2020-03-21 18:28:58 -0400

    completed more tests

--- src/main/java/rocks/zipcode/quiz4/objectorientation/account/Bank.java
+++ src/main/java/rocks/zipcode/quiz4/objectorientation/account/Bank.java
@@ -1,17 +1,23 @@
 package rocks.zipcode.quiz4.objectorientation.account;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author leon on 27/12/2018.
  */
 public class Bank {
+    List<BankAccount> allAccounts = new ArrayList<BankAccount>();
     public BankAccount removeBankAccountByIndex(Integer indexNumber) {
         return null;
     }
 
     public void addBankAccount(BankAccount bankAccount) {
+        allAccounts.add(bankAccount);
     }
 
     public Boolean containsBankAccount(BankAccount bankAccount) {
-        throw new UnsupportedOperationException("Method not yet implemented");
+
+        return allAccounts.contains(bankAccount);
     }
 }
