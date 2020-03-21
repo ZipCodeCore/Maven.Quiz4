78556d489f17ad56df9b139e56d62188f7cefa60
Mo Russ <mauriceruss302@gmail.com>
2020-03-20 21:55:23 -0400

    all bankaccount tests passed

--- src/main/java/rocks/zipcode/quiz4/objectorientation/account/BankAccount.java
+++ src/main/java/rocks/zipcode/quiz4/objectorientation/account/BankAccount.java
@@ -4,21 +4,30 @@ package rocks.zipcode.quiz4.objectorientation.account;
  * @author leon on 27/12/2018.
  */
 public class BankAccount extends Account implements Transactable{
+    private  double balance;
+
     public void setBalance(Double val) {
+        this.balance = val;
     }
 
     @Override
-    public void deposit(Double amountToIncreaseBy) {
-
+    public void deposit(Double amountToIncreaseBy) throws IllegalArgumentException {
+        if(amountToIncreaseBy < 0){
+            throw new IllegalArgumentException();
+        }
+        balance += amountToIncreaseBy;
     }
 
     @Override
-    public void withdrawal(Double amountToDecreaseBy) {
-
+    public void withdrawal(Double amountToDecreaseBy) throws IllegalArgumentException {
+        if(balance < amountToDecreaseBy || amountToDecreaseBy < 0){
+            throw new IllegalArgumentException();
+        }
+        balance -= amountToDecreaseBy;
     }
 
     @Override
     public Double getBalance() {
-        return null;
+        return balance;
     }
 }
