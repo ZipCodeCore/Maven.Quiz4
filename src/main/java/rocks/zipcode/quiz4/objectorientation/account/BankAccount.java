ca773da18a50170b4c3ae332912df7f7a15f5d12
Khalil Crumpler <crumplerkhalil@gmail.com>
2020-03-21 19:12:21 -0400

    finished more tests

--- src/main/java/rocks/zipcode/quiz4/objectorientation/account/BankAccount.java
+++ src/main/java/rocks/zipcode/quiz4/objectorientation/account/BankAccount.java
@@ -4,21 +4,24 @@ package rocks.zipcode.quiz4.objectorientation.account;
  * @author leon on 27/12/2018.
  */
 public class BankAccount extends Account implements Transactable {
+    private Double balance;
     public void setBalance(Double val) {
+        this.balance = val;
     }
 
     @Override
     public void deposit(Double amountToIncreaseBy) {
+        this.balance += amountToIncreaseBy;
 
     }
 
     @Override
     public void withdrawal(Double amountToDecreaseBy) {
-
+        this.balance -= amountToDecreaseBy;
     }
 
     @Override
     public Double getBalance() {
-        return null;
+        return this.balance;
     }
 }
