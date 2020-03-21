f7f43246b09a3db0a5f38a1bc86f4c4a0bdb40ad
Khalil Crumpler <crumplerkhalil@gmail.com>
2020-03-21 18:09:08 -0400

    more tests

--- src/main/java/rocks/zipcode/quiz4/collections/WordCounter.java
+++ src/main/java/rocks/zipcode/quiz4/collections/WordCounter.java
@@ -1,12 +1,24 @@
 package rocks.zipcode.quiz4.collections;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class WordCounter {
+    Map<String, Integer> wordMap = new HashMap<>();
     public WordCounter(String... strings) {
+        for(String word : strings){
+            Integer count = wordMap.get(word);
+            if(count == null){
+                wordMap.put(word, 1);
+            }
+            else{
+                wordMap.put(word, wordMap.get(word) + 1);
+            }
+        }
     }
 
     public Map<String, Integer> getWordCountMap() {
-        return null;
+
+        return wordMap;
     }
 }
--- src/main/java/rocks/zipcode/quiz4/collections/culonary/Food.java
+++ src/main/java/rocks/zipcode/quiz4/collections/culonary/Food.java
@@ -1,6 +1,7 @@
 package rocks.zipcode.quiz4.collections.culonary;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -15,6 +16,7 @@ public class Food {
     }
 
     public <SpiceType extends Class<? extends Spice>> Map<SpiceType, Integer> getSpiceCount() {
+
         return null;
     }
 
