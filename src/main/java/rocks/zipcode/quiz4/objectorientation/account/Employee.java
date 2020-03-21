4257b9b33f015d6383c2179937c68f1d953ffb8e
David Comer <50469980+David-B-Comer@users.noreply.github.com>
2020-03-20 23:12:22 -0400

    object done

--- src/main/java/rocks/zipcode/quiz4/objectorientation/account/Employee.java
+++ src/main/java/rocks/zipcode/quiz4/objectorientation/account/Employee.java
@@ -3,18 +3,66 @@ package rocks.zipcode.quiz4.objectorientation.account;
 /**
  * @author leon on 30/12/2018.
  */
-public class Employee {
+public class Employee implements Worker, Transactable {
+    private BankAccount bankAccount;
+    private Double hoursWorked;
+    private Double wage;
+
+
     public Employee() {
+        bankAccount = new BankAccount();
+        wage = 35.0;
+        hoursWorked = 0.0;
     }
 
     public Employee(BankAccount bankAccount) {
+        this.bankAccount = bankAccount;
+        this.wage = 35.0;
+        this.hoursWorked = 0.0;
     }
 
     public BankAccount getBankAccount() {
-        return null;
+        return bankAccount;
     }
 
     public void setBankAccount(BankAccount bankAccount) {
+        this.bankAccount = bankAccount;
 
+    }
+
+    @Override
+    public void deposit(Double amountToIncreaseBy) {
+        bankAccount.deposit(amountToIncreaseBy);
+    }
+
+    @Override
+    public void withdrawal(Double amountToDecreaseBy) {
+        bankAccount.withdrawal(amountToDecreaseBy);
+    }
+
+    @Override
+    public Double getBalance() {
+        return bankAccount.getBalance();
+    }
+
+    @Override
+    public void increaseHoursWorked(Double numberOfHours) {
+        hoursWorked += numberOfHours;
+
+    }
+
+    @Override
+    public Double getHoursWorked() {
+        return hoursWorked;
+    }
+
+    @Override
+    public Double getHourlyWage() {
+        return wage;
+    }
+
+    @Override
+    public Double getMoneyEarned() {
+        return hoursWorked * wage;
     }
 }
