ee7e59f0767862fd5c7ab172214b56587a6d2909
Khalil Crumpler <crumplerkhalil@gmail.com>
2020-03-21 16:46:20 -0400

    finished get all spices test

--- src/main/java/rocks/zipcode/quiz4/collections/culonary/Food.java
+++ src/main/java/rocks/zipcode/quiz4/collections/culonary/Food.java
@@ -1,5 +1,6 @@
 package rocks.zipcode.quiz4.collections.culonary;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -7,8 +8,10 @@ import java.util.Map;
  * @author leon on 27/12/2018.
  */
 public class Food {
+    List<Spice> spices = new ArrayList<>();
     public List<Spice> getAllSpices() {
-        return null;
+
+        return this.spices;
     }
 
     public <SpiceType extends Class<? extends Spice>> Map<SpiceType, Integer> getSpiceCount() {
@@ -16,5 +19,6 @@ public class Food {
     }
 
     public void applySpice(Spice spice) {
+        this.spices.add(spice);
     }
 }
