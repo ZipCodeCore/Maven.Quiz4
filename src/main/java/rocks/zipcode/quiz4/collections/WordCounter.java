1ac0707f7098e913d636a8365e79cb132eab60d3
Mo Russ <mauriceruss302@gmail.com>
2020-03-21 12:16:30 -0400

    all collections tests passed

--- src/main/java/rocks/zipcode/quiz4/collections/WordCounter.java
+++ src/main/java/rocks/zipcode/quiz4/collections/WordCounter.java
@@ -1,12 +1,23 @@
 package rocks.zipcode.quiz4.collections;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class WordCounter {
+    Map<String, Integer> wordCount = new HashMap<String, Integer>();
     public WordCounter(String... strings) {
-    }
 
-    public Map<String, Integer> getWordCountMap() {
-        return null;
+        for (String word : strings) {
+            if (!wordCount.containsKey(word)) {
+                wordCount.put(word, 1);
+            } else {
+                int count = wordCount.get(word);
+                wordCount.put(word, count + 1);
+            }
+        }
     }
+        public Map<String, Integer> getWordCountMap (){
+            return wordCount;
+        }
+
 }
