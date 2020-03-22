2e9d4bcad11efb7741d5e3d43818e0207f8fac18
asconem <asconem@gmail.com>
2020-03-22 17:14:19 -0400

    Finished

--- src/main/java/rocks/zipcode/quiz4/collections/culonary/Curry.java
+++ src/main/java/rocks/zipcode/quiz4/collections/culonary/Curry.java
@@ -1,4 +1,21 @@
 package rocks.zipcode.quiz4.collections.culonary;
 
-public class Curry {
+public class Curry implements Spice{
+    String name;
+
+    public Curry() {
+        this.name = null;
+    }
+
+    public Curry(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
